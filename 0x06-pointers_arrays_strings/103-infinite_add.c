8cb34b8fe4df996e7861625efec69cf2fa0e068d
EhmeeUd <iniememudosen@gmail.com>
2023-03-31 07:17:08 -0700

    failed task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,37 @@
 #include "holberton.h"
-#include <stdio.h>
-/**
- * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
- */
+#include <string.h>
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
-
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
-		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
-	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-		*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
-	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+    int carry = 0;
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    int max_len = (len1 > len2) ? len1 : len2;
+    
+    if (max_len + 1 > size_r) {
+        // Not enough space in r to store the result
+        return 0;
+    }
+    
+    r[max_len + 1] = '\0'; // Set the null terminator at the end of the result string
+    
+    for (int i = 0; i < max_len; i++) {
+        int d1 = (i < len1) ? n1[len1 - 1 - i] - '0' : 0; // Get the i-th digit from n1 (from the right)
+        int d2 = (i < len2) ? n2[len2 - 1 - i] - '0' : 0; // Get the i-th digit from n2 (from the right)
+        int sum = d1 + d2 + carry;
+        carry = sum / 10;
+        r[max_len - i] = (sum % 10) + '0'; // Set the i-th digit of the result (from the right)
+    }
+    
+    if (carry) {
+        // There's a carry left after the loop
+        if (max_len + 2 > size_r) {
+            // Not enough space in r to store the result with the carry
+            return 0;
+        }
+        r[0] = carry + '0'; // Set the leftmost digit of the result to the carry
+        return &r[0];
+    }
+    
+    return &r[1]; // Return a pointer to the result, skipping the leftmost 0 (if any)
 }
+
