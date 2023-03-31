136e3db167597cdad38f911cb57921de599341a3
EhmeeUd <iniememudosen@gmail.com>
2023-03-31 08:11:32 -0700

    Corrections

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,21 +8,16 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
-int _putchar(char c) {
-    return putchar(c);
-}
-
 int main(void) {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
     return 0;
 }
 
